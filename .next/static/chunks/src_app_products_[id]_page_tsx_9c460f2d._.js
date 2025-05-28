42272d32833a05657b0937e3be6ed8aff610fb22
kevingaitan10r <kevingaitan10r@gmail.com>
2025-05-27 20:01:01 -0500

    Add .next/static/chunks/src_app_products_[id]_page_tsx_9c460f2d._.js 1748394060855

---
+++ .next/static/chunks/src_app_products_[id]_page_tsx_9c460f2d._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_app_products_[id]_page_tsx_734e2802._.js"
+],
+    source: "dynamic"
+});
